520ba3d3c67f97c26eac7dcccbd41e6ff98de840
Axel Chanfrault <axel@id-meneo.com>
2016-02-01 15:38:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,17 +18,7 @@
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
-
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-
+ 
 				// tags anyone?
 				the_tags();
 			}
@@ -36,14 +26,14 @@
 		   <?php endwhile; ?>
 
 		<?php if (!is_singular()) : ?>
-			<div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
-			<div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
+			<div class="nav-previous alignleft"><?php next_posts_link( 'Article pr&eacute;c&eacute;dent' ); ?></div>
+			<div class="nav-next alignright"><?php previous_posts_link( 'Article suivant' ); ?></div>
 		<?php endif; ?>
 
 		<?php else : ?>
 
 		<div class="alert alert-info">
-		  <strong>No content in this loop</strong>
+		  <strong>Pas de contenu</strong>
 		</div>
 
 		<?php endif; ?>
