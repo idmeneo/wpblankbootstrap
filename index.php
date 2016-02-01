cd306a946c0cefa2adc81570b40923ea15bfd99e
Axel Chanfrault <axel@id-meneo.com>
2016-02-01 15:36:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<?php if(have_posts()) : ?>
 		   <?php while(have_posts()) : the_post(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php the_title('<h2>','</h2>'); ?>
+				<?php the_title('<h1>','</h1>'); ?>
 		 		<?php the_content(); ?>
 			</div>
 			<?php
